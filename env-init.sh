bce81963c570720a74ed73df7996a69f02f60afa
Jiri Koutny <jirkakoutny@gmail.com>
2019-10-26 07:40:11 +0200

    Better way to disable cached response

--- env-init.sh
+++ env-init.sh
@@ -3,7 +3,7 @@
 function cleanup { rm ./env-init-functions.sh; }
 trap cleanup EXIT
 
-curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/master/env-init-functions.sh?$(date +%s)" --silent -o env-init-functions.sh
+curl "https://raw.githubusercontent.com/DataSentics/dev-env-init/master/env-init-functions.sh?$(date +%s)" -H 'Cache-Control: no-cache' --silent -o env-init-functions.sh
 . "env-init-functions.sh"
 
 prepare_environment_for_package
