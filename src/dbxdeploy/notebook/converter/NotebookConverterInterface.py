8a7a3b13e3dfeaf59bec0f7f50473bf0cecbc74b
Jiri Koutny <jirkakoutny@gmail.com>
2020-02-16 18:34:37 +0100

    Interface implementation fixed

--- src/dbxdeploy/notebook/converter/NotebookConverterInterface.py
+++ src/dbxdeploy/notebook/converter/NotebookConverterInterface.py
@@ -1,22 +1,28 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from pathlib import Path, PurePosixPath
 
 class NotebookConverterInterface(ABC):
 
+    @abstractmethod
     def toDbcNotebook(self, notebookPath: Path, whlFilename: PurePosixPath) -> str:
         pass
 
+    @abstractmethod
     def toWorkspaceImportNotebook(self, notebookPath: Path, whlFilename: PurePosixPath) -> str:
         pass
 
+    @abstractmethod
     def resolves(self, fileExtension: str) -> bool:
         pass
 
+    @abstractmethod
     def getGlobPatterns(self) -> list:
         pass
 
+    @abstractmethod
     def getConsumerGlobPatterns(self) -> list:
         pass
 
+    @abstractmethod
     def getDescription(self) -> str:
         pass
