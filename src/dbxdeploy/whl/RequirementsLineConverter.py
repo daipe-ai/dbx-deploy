5dbd5d978c07b829d945ce176647ace1fd7524b2
Jiri Koutny <jirkakoutny@gmail.com>
2020-04-07 13:21:02 +0200

    support for git repo as the project dependency

--- src/dbxdeploy/whl/RequirementsLineConverter.py
+++ src/dbxdeploy/whl/RequirementsLineConverter.py
@@ -1,4 +1,5 @@
 import re
+from typing import Match
 from tomlkit import inline_table
 
 class RequirementsLineConverter:
@@ -6,9 +7,17 @@ class RequirementsLineConverter:
     def parse(self, line: str):
         matches = re.match(r'^([^=]+)==([^;]+)(?:; (.+))?$', line)
 
-        if not matches:
-            raise Exception('Invalid requirements.txt line: {}'.format(line))
+        if matches:
+            return self.__parseLine(matches)
 
+        matches = re.match(r'^-e git\+(git@[^@]+)@([^#]+)#egg=(.+)$', line)
+
+        if matches:
+            return self.__parseGitLine(matches)
+
+        raise Exception('Invalid requirements.txt line: {}'.format(line))
+
+    def __parseLine(self, matches: Match[str]):
         if matches.group(3) is None:
             return [matches.group(1), matches.group(2).strip()]
 
@@ -17,3 +26,10 @@ class RequirementsLineConverter:
         it.append('markers', matches.group(3))
 
         return [matches.group(1), it]
+
+    def __parseGitLine(self, matches: Match[str]):
+        it = inline_table()
+        it.append('git', matches.group(1))
+        it.append('rev', matches.group(2))
+
+        return [matches.group(3), it]
--- src/dbxdeploy/whl/RequirementsLineConverterTest.py
+++ src/dbxdeploy/whl/RequirementsLineConverterTest.py
@@ -21,5 +21,10 @@ class RequirementsLineConverterTest(unittest.TestCase):
 
         self.assertEqual(['pywin32', {'version': '227', 'markers': 'sys_platform == "win32" and python_version >= "3.6" or sys_platform == "win32"'}], result)
 
+    def test_gitClone(self):
+        result = self.__requirementsLineConverter.parse('-e git+git@github.com:myaccount/myrepo.git@82f1bb9de9665918c262465e72e872e4f744112c#egg=my-package-name')
+
+        self.assertEqual(['my-package-name', {'git': 'git@github.com:myaccount/myrepo.git', 'rev': '82f1bb9de9665918c262465e72e872e4f744112c'}], result)
+
 if __name__ == '__main__':
     unittest.main()
