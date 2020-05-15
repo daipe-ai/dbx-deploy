e4ad43be5c0c972cab5b603981a6d355f30172ea
Jiri Koutny <jirkakoutny@gmail.com>
2020-05-15 16:44:07 +0200

    compatibility with injecta 0.8 & pyfony 0.5

--- src/dbxdeploy/dbc/CommandsConverterTest.py
+++ src/dbxdeploy/dbc/CommandsConverterTest.py
@@ -1,5 +1,6 @@
 import unittest
 from dbxdeploy.containerInit import initContainer
+from dbxdeploy.dbc.CommandConverter import CommandConverter
 from dbxdeploy.dbc.CommandsConverter import CommandsConverter
 from dbxdeploy.notebook.converter.DatabricksNotebookConverter import DatabricksNotebookConverter
 
@@ -7,24 +8,12 @@ class CommandsConverterTest(unittest.TestCase):
 
     def setUp(self):
         container = initContainer('test')
+        self.__commandConverter = container.get(CommandConverter) # type: CommandConverter
 
-        self.__commandsConverter = container.get(CommandsConverter) # type: CommandsConverter
+    def test_forcedEndFileNewLine(self):
+        result = self.__createResult(True)
 
-    def test_basic(self):
-        commands = [
-            {'command': 'print("Hello world")', 'position': 2, 'commandTitle': ''},
-            {'command': '%run /foo/bar', 'position': 1, 'commandTitle': ''},
-            {'command': '', 'position': 1.33, 'commandTitle': ''},
-            {'command': '', 'position': 1.66, 'commandTitle': ''}
-        ]
-
-        result = self.__commandsConverter.convert(
-            commands,
-            DatabricksNotebookConverter.firstLine,
-            DatabricksNotebookConverter.cellSeparator,
-        )
-
-        self.assertNotebook([
+        self.__assertNotebook([
             '# Databricks notebook source',
             '# MAGIC %run /foo/bar',
             '',
@@ -34,7 +23,35 @@ class CommandsConverterTest(unittest.TestCase):
             '',
         ], result)
 
-    def assertNotebook(self, expected: list, resultCode: str):
+    def test_noForcedEndFileNewLine(self):
+        result = self.__createResult(False)
+
+        self.__assertNotebook([
+            '# Databricks notebook source',
+            '# MAGIC %run /foo/bar',
+            '',
+            '# COMMAND ----------',
+            '',
+            'print("Hello world")',
+        ], result)
+
+    def __createResult(self, forceEndFileNewLine: bool):
+        commandsConverter = CommandsConverter(forceEndFileNewLine, self.__commandConverter) # type: CommandsConverter
+
+        commands = [
+            {'command': 'print("Hello world")', 'position': 2, 'commandTitle': ''},
+            {'command': '%run /foo/bar', 'position': 1, 'commandTitle': ''},
+            {'command': '', 'position': 1.33, 'commandTitle': ''},
+            {'command': '', 'position': 1.66, 'commandTitle': ''}
+        ]
+
+        return commandsConverter.convert(
+            commands,
+            DatabricksNotebookConverter.firstLine,
+            DatabricksNotebookConverter.cellSeparator,
+        )
+
+    def __assertNotebook(self, expected: list, resultCode: str):
         resultLines = resultCode.split('\n')
 
         self.assertEqual(expected, resultLines)
