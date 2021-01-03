92bea6af2498cfb6ad4aa657f8b9929ad2dd22ae
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-03 10:41:20 +0100

    dev-env-init -> penvy

--- env-init.sh
+++ env-init.sh
@@ -1,17 +1,42 @@
 #!/bin/bash -e
 
-function cleanup {
-  rv=$?
-  rm ./dev_env_init.sh
-  exit $rv
+PENVY_VERSION="1.0.4"
+POSSIBLE_PATHS_LIST_URL="https://raw.githubusercontent.com/pyfony/penvy/master/src/penvy/conda/conda_executable_paths.txt"
+
+resolve_conda_executable_path() {
+  if hash conda 2>/dev/null; then
+    echo "Using conda from PATH"
+
+    CONDA_EXECUTABLE_PATH="conda"
+  else
+    POSSIBLE_PATHS_LIST=$(curl --silent $POSSIBLE_PATHS_LIST_URL)
+
+    while IFS= read -r line; do
+      FILE_PATH=$(sed -E "s|\~|$HOME|g" <<< $line)
+
+      if [ -f "$FILE_PATH" ]; then
+        echo "Using conda from: $FILE_PATH"
+        CONDA_EXECUTABLE_PATH="$FILE_PATH"
+        break
+      fi
+    done <<< "$POSSIBLE_PATHS_LIST"
+
+    if [ -z ${CONDA_EXECUTABLE_PATH+x} ]; then
+      echo "Unable to resolve Conda executable path"
+      exit 1
+    fi
+  fi
 }
-trap cleanup EXIT
 
-if [[ -z "$ENV_INIT_BRANCH" ]]; then ENV_INIT_BRANCH="master"; fi
+resolve_conda_executable_path
 
-echo "dev-env-init branch: $ENV_INIT_BRANCH"
+CONDA_BASE_DIR=$($CONDA_EXECUTABLE_PATH info --base | sed 's/\\/\//g')
 
-curl "https://raw.githubusercontent.com/pyfony/dev-env-init/$ENV_INIT_BRANCH/dev_env_init.sh?$(date +%s)" -H 'Cache-Control: no-cache' --silent -o dev_env_init.sh
-. "dev_env_init.sh"
+if [ -d "$CONDA_BASE_DIR/Scripts" ]; then
+  CONDA_BIN_DIR="$CONDA_BASE_DIR/Scripts" # Windows
+else
+  CONDA_BIN_DIR="$CONDA_BASE_DIR/bin" # Linux/Mac
+fi
 
-prepare_environment_for_package
+$CONDA_BIN_DIR/pip install "penvy==$PENVY_VERSION"
+$CONDA_BIN_DIR/penvy-init "$@"
