6b06b3044cc0e64aa0a2dad5a7edbed5f6b776d5
Jiri Koutny <jirkakoutny@gmail.com>
2021-02-05 13:08:35 +0100

    "Deployed to" log message added

--- src/dbxdeploy/deploy/DeployCommand.py
+++ src/dbxdeploy/deploy/DeployCommand.py
@@ -1,5 +1,6 @@
 import asyncio
 from argparse import Namespace
+from logging import Logger
 from dbxdeploy.deploy.Deployer import Deployer
 from consolebundle.ConsoleCommand import ConsoleCommand
 
@@ -7,16 +8,24 @@ class DeployCommand(ConsoleCommand):
 
     def __init__(
         self,
+        dbxHost: str,
+        workspaceBaseDir: str,
+        logger: Logger,
         deployer: Deployer
     ):
+        self.__dbxHost = dbxHost
+        self.__workspaceBaseDir = workspaceBaseDir
+        self.__logger = logger
         self.__deployer = deployer
 
     def getCommand(self) -> str:
         return 'dbx:deploy'
 
     def getDescription(self):
-        return 'Deploy to DBX'
+        return 'Deploy notebooks and master package to DBX'
 
     def run(self, inputArgs: Namespace):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.__deployer.deploy())
+
+        self.__logger.info(f'Deployed to {self.__dbxHost}, {self.__workspaceBaseDir}')
--- src/dbxdeploy/deploy/ReleaseCommand.py
+++ src/dbxdeploy/deploy/ReleaseCommand.py
@@ -1,5 +1,6 @@
 import asyncio
 from argparse import Namespace
+from logging import Logger
 from dbxdeploy.deploy.Releaser import Releaser
 from consolebundle.ConsoleCommand import ConsoleCommand
 
@@ -7,8 +8,14 @@ class ReleaseCommand(ConsoleCommand):
 
     def __init__(
         self,
+        dbxHost: str,
+        workspaceBaseDir: str,
+        logger: Logger,
         releaser: Releaser
     ):
+        self.__dbxHost = dbxHost
+        self.__workspaceBaseDir = workspaceBaseDir
+        self.__logger = logger
         self.__releaser = releaser
 
     def getCommand(self) -> str:
@@ -20,3 +27,5 @@ class ReleaseCommand(ConsoleCommand):
     def run(self, inputArgs: Namespace):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.__releaser.release())
+
+        self.__logger.info(f'Deployed to {self.__dbxHost}, {self.__workspaceBaseDir}')
