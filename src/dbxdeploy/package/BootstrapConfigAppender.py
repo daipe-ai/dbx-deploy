437470c07cb40ff9d521f63c3344450dc5ce6753
Lukas Langr <lukas.langr@datasentics.com>
2021-04-26 11:29:45 +0200

    Fix BootstrapConfigAppender for DBR 8

--- src/dbxdeploy/package/BootstrapConfigAppender.py
+++ src/dbxdeploy/package/BootstrapConfigAppender.py
@@ -9,19 +9,17 @@ class BootstrapConfigAppender:
     def append(self, package_path: Path):
         package_file_name = package_path.stem
         package_file_name_parts = package_file_name.split("-")
-        package_file_name = package_file_name_parts[0].replace(
-            "__", "_"
-        )  # __myproject__ needs to be converted to _myproject_ for some reason
+        package_name = package_file_name_parts[0].replace("__", "_")  # __myproject__ needs to be converted to _myproject_ for some reason
 
-        dist_info_dir = f"{package_file_name}-{package_file_name_parts[1]}.dist-info"
+        dist_info_dir = f"{package_name}-{package_file_name_parts[1]}.dist-info"
 
         zip_file = zipfile.ZipFile(package_path, "a")
-        zip_file.writestr(dist_info_dir + "/entry_points.txt", self.__prepare_entry_points())
+        zip_file.writestr(dist_info_dir + "/entry_points.txt", self.__prepare_entry_points(package_name))
         zip_file.writestr(dist_info_dir + "/bootstrap_config.json", self.__prepare_bootstrap_config())
         zip_file.close()
 
-    def __prepare_entry_points(self):
-        return "[pyfony.bootstrap]\nfoo=bar\n"
+    def __prepare_entry_points(self, package_name):
+        return f"[pyfony.bootstrap]\npackage_name={package_name}\n"
 
     def __prepare_bootstrap_config(self):
         bootstrap_config = raw_config_reader.read(pyproject.get_path())
