ed1af28eff837ad515b3411ae07d3d82ec41ebdd
stano <stanislav.zoldak@datasentics.com>
2021-10-22 18:00:31 +0200

    Added duplicates check to repo pull

--- src/dbxdeploy/repo/RepoPullCommand.py
+++ src/dbxdeploy/repo/RepoPullCommand.py
@@ -33,13 +33,27 @@ class RepoPullCommand(ConsoleCommand):
     def configure(self, argument_parser: ArgumentParser):
         argument_parser.add_argument("--repo-url", dest="repo_url", help="Project repo url")
 
-    def _check_correct_project(self, repo_id, repo_url):
+    def __check_correct_project(self, repo_id, repo_url):
         repo = self.__repos_api.get(repo_id)
         if repo_url != repo["url"]:
             raise Exception(f"Repo at {repo['path']} has source at {repo['url']}, expected {repo_url}")
 
+    def __check_for_duplicates(self, repo_to_be_created: bool):
+        repo_list = self.__repos_api.list(self.__repo_root_dir, None)["repos"]
+        repo_branches = [repo["branch"] for repo in repo_list]
+        if repo_to_be_created:
+            repo_branches.append(self.__current_branch_resolver.resolve())
+        branch_counter = [{"name": branch, "count": repo_branches.count(branch)} for branch in set(repo_branches)]
+        duplicated_branches = [branch["name"] for branch in branch_counter if branch["count"] > 1]
+
+        if len(duplicated_branches) > 0:
+            message = f"Following branches are duplicated in current env: {', '.join(duplicated_branches)}."
+            if repo_to_be_created:
+                message = f"Can't create repo. Following branches would be duplicated in current env: {', '.join(duplicated_branches)}."
+            raise Exception(message)
+
     @staticmethod
-    def _get_provider_from_url(url):
+    def __get_provider_from_url(url):
         provider_map = {
             "github.com": "gitHub",
             "dev.azure.com": "azureDevOpsServices",
@@ -60,12 +74,14 @@ class RepoPullCommand(ConsoleCommand):
 
         try:
             env_repo_id = self.__repos_api.get_repo_id(self.__repo_path)
-            self._check_correct_project(env_repo_id, repo_url)
+            self.__check_for_duplicates(repo_to_be_created=False)
+            self.__check_correct_project(env_repo_id, repo_url)
         except RuntimeError:
             self.__logger.info("Repo not found, cloning new repo")
+            self.__check_for_duplicates(repo_to_be_created=True)
             env_repo_id = self.__repos_api.create(
                 url=repo_url,
-                provider=self._get_provider_from_url(repo_url),
+                provider=self.__get_provider_from_url(repo_url),
                 path=self.__repo_path
             )["id"]
         self.__repos_api.update(repo_id=env_repo_id, branch=branch, tag=None)
