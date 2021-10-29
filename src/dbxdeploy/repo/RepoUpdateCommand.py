9dfb6f9a8e75d5bdfd7afefb6838831aa3d0fe54
stano <stanislav.zoldak@datasentics.com>
2021-10-29 16:25:38 +0200

    Added required arguments to repo update

--- src/dbxdeploy/repo/RepoUpdateCommand.py
+++ src/dbxdeploy/repo/RepoUpdateCommand.py
@@ -12,17 +12,15 @@ class RepoUpdateCommand(ConsoleCommand):
         self,
         repos_api: ReposApi,
         workspace_api: WorkspaceApi,
-        current_branch_resolver: CurrentBranchResolver,
         logger: Logger,
         repo_root_dir: str,
         repo_path: str,
     ):
         self.__repos_api = repos_api
         self.__workspace_api = workspace_api
-        self.__branch = current_branch_resolver.resolve()
         self.__logger = logger
         self.__repo_root_dir = "/" + repo_root_dir.strip("/") + "/"
-        self.__repo_path = repo_path.format(current_branch=self.__branch)
+        self.__repo_path = repo_path
 
     def get_command(self) -> str:
         return "dbx:repo:update"
@@ -32,24 +30,24 @@ class RepoUpdateCommand(ConsoleCommand):
 
     def configure(self, argument_parser: ArgumentParser):
         argument_parser.add_argument("--repo-url", dest="repo_url", help="Project repo url")
+        argument_parser.add_argument("--checkout-branch", dest="checkout_branch", help="Branch to checkout")
+        argument_parser.add_argument("--repo-name", dest="repo_name", help="Project repo name")
 
     def __check_correct_project(self, repo_id, repo_url):
         repo = self.__repos_api.get(repo_id)
         if repo_url != repo["url"]:
             raise Exception(f"Repo at {repo['path']} has source at {repo['url']}, expected {repo_url}")
 
-    def __check_for_duplicates(self, repo_to_be_created: bool):
+    def __check_for_duplicates(self, with_branch: str = None):
         repo_list = self.__repos_api.list(self.__repo_root_dir, None).get("repos", [])
         repo_branches = [repo["branch"] for repo in repo_list]
-        if repo_to_be_created:
-            repo_branches.append(self.__branch)
+        if with_branch:
+            repo_branches.append(with_branch)
         branch_counter = [{"name": branch, "count": repo_branches.count(branch)} for branch in set(repo_branches)]
         duplicated_branches = [branch["name"] for branch in branch_counter if branch["count"] > 1]
 
         if len(duplicated_branches) > 0:
-            message = f"Following branches are duplicated in current env: {', '.join(duplicated_branches)}."
-            if repo_to_be_created:
-                message = f"Can't create repo. Following branches would be duplicated in current env: {', '.join(duplicated_branches)}."
+            message = f"Following branches are/would be duplicated in current env: {', '.join(duplicated_branches)}."
             raise Exception(message)
 
     @staticmethod
@@ -64,23 +62,27 @@ class RepoUpdateCommand(ConsoleCommand):
             raise Exception(f"Git provider for {url} not listed.")
 
     def run(self, input_args: Namespace):
-        repo_url = input_args.repo_url
+        if not (input_args.repo_url and input_args.checkout_branch and input_args.repo_name):
+            raise Exception("All arguments are required. Check -h for the list of arguments.")
 
-        self.__logger.info(f"Branch: {self.__branch}")
+        repo_url = input_args.repo_url
+        checkout_branch = input_args.checkout_branch
+        repo_path = self.__repo_path.format(current_branch=input_args.repo_name)
 
         self.__workspace_api.mkdirs(self.__repo_root_dir)
 
         try:
-            env_repo_id = self.__repos_api.get_repo_id(self.__repo_path)
-            self.__check_for_duplicates(repo_to_be_created=False)
+            env_repo_id = self.__repos_api.get_repo_id(repo_path)
+            self.__check_for_duplicates()
             self.__check_correct_project(env_repo_id, repo_url)
         except RuntimeError:
             self.__logger.info("Repo not found, cloning new repo")
-            self.__check_for_duplicates(repo_to_be_created=True)
+            self.__check_for_duplicates(with_branch=checkout_branch)
             env_repo_id = self.__repos_api.create(
                 url=repo_url,
                 provider=self.__get_provider_from_url(repo_url),
-                path=self.__repo_path
+                path=repo_path
             )["id"]
-        self.__repos_api.update(repo_id=env_repo_id, branch=self.__branch, tag=None)
+
+        self.__repos_api.update(repo_id=env_repo_id, branch=checkout_branch, tag=None)
         self.__logger.info("Repo successfully pulled")
