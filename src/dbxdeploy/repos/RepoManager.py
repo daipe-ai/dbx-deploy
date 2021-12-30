2f45e5f8917d802c53a3d3d41d3ac32319f93438
kutny <jirkakoutny@gmail.com>
2021-12-30 10:58:12 +0100

    dbx-deploy deploy paths fix

--- src/dbxdeploy/repos/RepoManager.py
+++ src/dbxdeploy/repos/RepoManager.py
@@ -1,6 +1,7 @@
+import os
 import urllib3
-from requests.exceptions import HTTPError
 from logging import Logger
+from requests.exceptions import HTTPError
 from databricks_cli.repos.api import ReposApi
 from databricks_cli.workspace.api import WorkspaceApi
 
@@ -10,36 +11,41 @@ class RepoManager:
         self,
         repo_root_dir: str,
         repo_path: str,
+        repo_base_dir: str,
         logger: Logger,
         repos_api: ReposApi,
         workspace_api: WorkspaceApi,
     ):
+        if repo_root_dir is not None or repo_path is not None:
+            raise Exception(
+                "dbxdeploy.target.repo.root_dir and dbxdeploy.target.repo.path are deprecated, please use dbxdeploy.target.repo.base_dir instead"
+            )
+
+        self.__repo_base_dir = repo_base_dir
+        self.__logger = logger
         self.__repos_api = repos_api
         self.__workspace_api = workspace_api
-        self.__logger = logger
-        self.__repo_root_dir = repo_root_dir
-        self.__repo_path = repo_path
 
-    def create_or_update(self, repo_url, branch, tag, repo_name, force):
-        repo_path = self.__repo_path.format(repo_name=repo_name)
+    def create_or_update(self, repo_url, repo_name=None, branch=None, tag=None, force=False):
+        repo_path = self.__resolve_repo_path(repo_name, branch, tag)
+        repo_group_path = os.path.split(repo_path)[0]
         self.__logger.info(f"Updating repo at {repo_path}")
-        self.__workspace_api.mkdirs(self.__repo_root_dir)
+        self.__workspace_api.mkdirs(repo_group_path)
 
-        repo_list = self.__repos_api.list(self.__repo_root_dir, None).get("repos", [])
+        repo_list = self.__repos_api.list(repo_group_path, None).get("repos", [])
+        self.__check_duplicated_branches(repo_list)
 
         try:
             repo_id = self.__repos_api.get_repo_id(repo_path)
-            self.__check_duplicates(repo_list)
+        except RuntimeError:
+            self.__logger.info("Repo not found, cloning new repo")
+            repo_id = self.__repos_api.create(url=repo_url, provider=self.__get_provider_from_url(repo_url), path=repo_path)["id"]
+        else:
             self.__check_correct_project(repo_id, repo_url)
             if force:
                 self.__logger.info("Re-cloning repo")
                 self.__repos_api.delete(repo_id)
                 repo_id = self.__repos_api.create(url=repo_url, provider=self.__get_provider_from_url(repo_url), path=repo_path)["id"]
-        except RuntimeError:
-            self.__logger.info("Repo not found, cloning new repo")
-            self.__check_duplicates(repo_list)
-            self.__check_valid_branch(repo_list, branch)
-            repo_id = self.__repos_api.create(url=repo_url, provider=self.__get_provider_from_url(repo_url), path=repo_path)["id"]
 
         try:
             self.__repos_api.update(repo_id=repo_id, branch=branch, tag=tag)
@@ -48,39 +54,43 @@ class RepoManager:
                 self.__logger.info("Error occurred, cleaning up ...")
                 self.__repos_api.delete(repo_id)
             raise e
+        else:
+            self.__logger.info("Repo successfully updated")
 
-        self.__logger.info("Repo successfully updated")
-
-    def delete(self, repo_url, repo_name):
-        repo_path = self.__repo_path.format(repo_name=repo_name)
-
+    def delete(self, repo_url, repo_name=None, branch=None, tag=None):
+        repo_path = self.__resolve_repo_path(repo_name, branch, tag)
         self.__logger.info(f"Deleting repo at {repo_path}")
 
         try:
             repo_id = self.__repos_api.get_repo_id(repo_path)
-            self.__check_correct_project(repo_id, repo_url)
         except RuntimeError:
             self.__logger.info("Repo not found, skipping delete")
         else:
+            self.__check_correct_project(repo_id, repo_url)
             self.__repos_api.delete(repo_id)
             self.__logger.info("Repo successfully deleted")
+
+    def __resolve_repo_path(self, repo_name, branch, tag):
+        self.__validate_placeholder_arguments("{repo_name}", repo_name, "--repo_name")
+        self.__validate_placeholder_arguments("{branch_name}", branch, "--branch")
+        self.__validate_placeholder_arguments("{tag_name}", tag, "--tag")
+        return self.__repo_base_dir.format(repo_name=repo_name, branch_name=branch, tag_name=tag)
+
+    def __validate_placeholder_arguments(self, placeholder, argument, argument_name):
+        if argument is None and placeholder in self.__repo_base_dir:
+            raise Exception(f"{placeholder} used in repo.base_dir, but {argument_name} argument was not provided")
 
     def __check_correct_project(self, repo_id, repo_url):
         repo = self.__repos_api.get(repo_id)
         if repo_url != repo["url"]:
             raise Exception(f"Repo at {repo['path']} has source at {repo['url']}, expected {repo_url}")
 
-    def __check_duplicates(self, repo_list: list):
+    def __check_duplicated_branches(self, repo_list: list):
         repo_branches = [repo.get("branch") for repo in repo_list]
         branch_counter = [{"name": branch, "count": repo_branches.count(branch)} for branch in set(repo_branches)]
         duplicated_branches = [branch["name"] for branch in branch_counter if branch["count"] > 1]
         if len(duplicated_branches) > 0:
             raise Exception(f"Following branches are duplicated in current env: {', '.join(duplicated_branches)}.")
-
-    def __check_valid_branch(self, repo_list: list, branch: str):
-        repo_branches = [repo.get("branch") for repo in repo_list]
-        if branch in repo_branches:
-            raise Exception(f"Branch {branch} is already present in current env.")
 
     def __get_provider_from_url(self, url):
         provider_map = {
