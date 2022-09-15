2ff34b07c5438e47a77143afe3ea3b53653e95f4
Karel Gudera <karel.gudera@gmail.com>
2022-09-15 21:51:27 +0200

    Fix non-existent git repository exception at container bootstrap time

--- src/dbxdeploy/git/CurrentBranchResolver.py
+++ src/dbxdeploy/git/CurrentBranchResolver.py
@@ -1,12 +1,18 @@
-from dbxdeploy.git.CurrentRepositoryFactory import CurrentRepositoryFactory
+from pathlib import Path
+from pygit2 import Repository, discover_repository, GitError  # pyre-ignore  # pylint: disable = no-name-in-module
 
 
 class CurrentBranchResolver:
     def __init__(
         self,
-        current_repository_factory: CurrentRepositoryFactory,
+        base_path: Path,
     ):
-        self.__current_repository_factory = current_repository_factory
+        self.__base_path = base_path
 
-    def resolve(self) -> str:
-        return self.__current_repository_factory.create().head.shorthand
+    def resolve(self):
+        repository_path = discover_repository(self.__base_path.as_posix())
+
+        if not repository_path:
+            raise GitError(f'No repository found at "{self.__base_path}" and its parents')
+
+        return Repository(repository_path).head.shorthand
